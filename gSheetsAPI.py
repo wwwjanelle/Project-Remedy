b005139ea6ba724d2b9f09cc9a88036bcd4ad2c1
Janelle Wallace <janelleawallace@gmail.com>
2023-02-13 00:07:17 -0500

    Convert spreadsheet to data frame

--- gSheetsAPI.py
+++ gSheetsAPI.py
@@ -1,7 +1,8 @@
 import gspread
 import pandas as pd
 
-gc = gspread.service_account('credentials.json') #credentials.json is currently untracked in this repo
+gc = gspread.service_account('credentials.json')
 spreadsheet = gc.open_by_url('https://docs.google.com/spreadsheets/d/1EmMMQuwTp7buaYPakON1FlcFOJMAgrKKNA0XJwaIHPg/edit#gid=0')
 
-print(spreadsheet.sheet1.get())
+df = pd.DataFrame(spreadsheet.sheet1.get())
+print(df.head())
