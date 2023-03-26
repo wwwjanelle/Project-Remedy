187f467a23a59b4a4c57ca71ec1b9eb052f3d07f
kevinjr0123 <81383919+kevinjr0123@users.noreply.github.com>
2023-03-26 18:47:24 -0400

    code change

--- remedy-website-react-ui/src/pages/Other/Survey.js
+++ remedy-website-react-ui/src/pages/Other/Survey.js
@@ -4,31 +4,15 @@ import "../../App.css";
 // import { Link } from 'react-router-dom';
 
 export default class Survey extends Component {
-    render() {
-        var user = JSON.parse(localStorage.getItem('user'));
-        
+    render() {     
         return (
             <form>
 
-                <h2>Hello, { user } </h2>
 
-                <button type="submit" className="btn btn-dark btn-lg btn-block">Home </button>
-                <button type="submit" className="btn btn-dark btn-lg btn-block">New Survey</button>
-                <button type="submit" className="btn btn-dark btn-lg btn-block">My Account</button>
-
-                <h3>Surveys</h3>
+                <h3>Survey</h3>
 
                 <p>patient sees past surveys...</p>
 
-                <button type="submit" className="btn btn-dark btn-lg btn-block">Past Surveys</button>
-
-                <h3>Assessments</h3>
-
-                <button type="submit" className="btn btn-dark btn-lg btn-block">Patient Assessments</button>
-
-                <h3>My Providers</h3>
-
-                <button type="submit" className="btn btn-dark btn-lg btn-block">Patient's Providers</button>
             </form>
         );
     }
