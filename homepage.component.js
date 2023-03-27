80a4481fa74e1e4adadbe433ebc4443c58a06dbf
Kuira Edwards <kuiraedwards@gmail.com>
2023-03-27 15:47:24 -0400

    Home Page Changes

--- homepage.component.js
+++ homepage.component.js
@@ -4,6 +4,7 @@ export default class HomePage extends Component {
     render() {
         return (
             <form>
+                <img border = "0" src="Remedy-xxx" alt="Remedy" width="64" height="64"/>
                 <h3>Home Page</h3>
 
                 <button type="submit" className="btn btn-dark btn-lg btn-block">Provider</button>
--- remedy-website-react-ui/src/pages/Home/Homepage.js
+++ remedy-website-react-ui/src/pages/Home/Homepage.js
@@ -1,14 +1,29 @@
 import React, { useState } from "react";
 import "../../App.css";
+import image from "../../img/Remedy-1.jpeg";
+import background from "../../img/Background.png";
 // import { Button } from "@mui/material";
+// <script>
+// function bigImg(x) {
+//    x.style.height = "128px";
+//    x.style.width = "128px";
+//}
+//function normalImg(x) {
+//    x.style.height = "64px";
+//    x.style.width = "65px";
+//}
+//</script>
 
 const HomePage = ({ }) => {
 
     return (
         <form>
+            <div style={{backgroundImage: {background}, backgroundRepeat:"no-repeat"}}></div>
+            <img border = "0" src={image} alt="Remedy" width="64" height="64"/>
             <h3>Home Page</h3>
             <button type="submit" className="btn btn-dark btn-lg btn-block"><a href="/provider">Provider</a></button>
             <button type="submit" className="btn btn-dark btn-lg btn-block"><a href="/patient">Patient</a></button>
+            
         </form>
     );
 };
