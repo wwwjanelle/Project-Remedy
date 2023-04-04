29259e15769af7e8b92a557bff661ce8c5fa5616
Kuira Edwards <kuiraedwards@gmail.com>
2023-04-04 14:46:23 -0400

    UI Changes

--- remedy-website-react-ui/src/pages/Provider/Provider.js
+++ remedy-website-react-ui/src/pages/Provider/Provider.js
@@ -1,16 +1,31 @@
 import React, { Component } from "react";
 import "../../App.css";
+import image from "../../img/trees.jpeg";
 
 export default class Provider extends Component {
     render() {
         return (
+            <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
+            <div className='hidden sm:block'>
+                <img className='w-50 h-50 object-cover' src={image} alt="Remedy" />
+            </div>
+
+            <div className='bg-gray-100 flex flex-col justify-center'>
+                <form className='max-w-[400px] w-full mx-auto bg-white p-4'>
+                    <h2 className='text-4xl font-bold text-center py-6'>PROVIDER</h2>
+                    <button type="submit" className="border shadow-lg hover:shadow-xl px-15 py-2 relative flex items-center"><a href="/provider/login">SIGN IN</a></button>
+                    <button type="submit" className="border shadow-lg hover:shadow-xl px-15 py-2 relative flex items-center"><a href="/provider/signup">SIGN UP</a></button>
+                </form>
+            </div>
+        </div>
+            /*
             <form>
                 <h3>Provider</h3>
 
-                <button type="submit" className="btn btn-dark btn-lg btn-block"><a href="/provider/login">Log In</a></button>
-                <button type="submit" className="btn btn-dark btn-lg btn-block"><a href="/provider/signup">Sign Up</a></button>
+                <button type="button" className="btn btn-dark btn-lg btn-block"><a href="/provider/login">Log In</a></button>
+                <button type="button" className="btn btn-dark btn-lg btn-block"><a href="/provider/signup">Sign Up</a></button>
 
-            </form>
+            </form> */
 
         );
     }
--- remedy-website-react-ui/src/pages/Provider/SignUp.js
+++ remedy-website-react-ui/src/pages/Provider/SignUp.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { useNavigate } from 'react-router-dom';
 import { auth, db } from '../../firebase.js';
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import "../../App.css";
+import image2 from "../../img/tree.jpeg";
+import image from "../../img/google.webp";
 
 const SignUp = ({setShouldShowSignIn}) => {
     const [name, setName] = useState("");
@@ -77,8 +80,82 @@ const SignUp = ({setShouldShowSignIn}) => {
         setHospitalID("");
         setPhoneNum("");
     };
+    const doGoogleSignUp = () => {
+        setSignUpError(null);
+        const provider = new GoogleAuthProvider();
+        signInWithPopup(auth, provider)
+        .then((result) => {
+          console.log(result);
+        }).catch((error) => {
+          console.log(error);
+          setSignUpError(genericError);
+        });
+    }
 
     return (
+        <div className='relative w-full h-screen bg-zinc-900/90'>
+        <img className='absolute w-full h-full object-cover mix-blend-overlay' src={image2} alt="/" />
+        {signUpError ? <div className="log-sign-error">{signUpError}</div> : <></>}
+            {error && <ErrorMessage>Please fill all the fields</ErrorMessage>};
+
+        <div className='flex justify items-center h-full'>
+            <form className='max-w-[400px] w-full mx-auto bg-white p-8'onSubmit={handleSubmit}>
+                <h2 className='text-4xl font-bold text-center py-4'>REGISTER</h2>
+                <div className='flex justify-between py-8'> 
+                </div>
+                <div className='flex flex-col mb-4'>
+                    <label>Full Name</label>
+                    <input className='border relative bg-gray-100 p-2' type="text" value={name}
+                        onChange={(e) => setName(e.target.value)}/>
+                </div>
+                <div className='flex flex-col mb-4'>
+                    <label>Email</label>
+                    <input className='border relative bg-gray-100 p-2' type="text" value={email}
+                        onChange={(e) => setEmail(e.target.value)}/>
+                </div>
+                <div className='flex flex-col mb-4'>
+                    <label>Hospital ID</label>
+                    <input className='border relative bg-gray-100 p-2' type="text" value={hospitalID}
+                        onChange={(e) => setHospitalID(e.target.value)}/>
+                </div>
+                <div className='flex flex-col mb-4'>
+                    <label>Phone Number</label>
+                    <input className='border relative bg-gray-100 p-2' type="text" value={phoneNum}
+                        onChange={(e) => setPhoneNum(e.target.value)}/>
+                </div>
+                <div className='flex flex-col mb-4'>
+                    <label>Password (at least 6 characters)</label>
+                    <input className='border relative bg-gray-100 p-2' type="text" value={password}
+                        onChange={(e) => setPassword(e.target.value)}/>
+                </div>
+                <div className='flex flex-col mb-4'>
+                    <label>Confirm Password</label>
+                    <input className='border relative bg-gray-100 p-2' type="text" value={confirmPass}
+                        onChange={(e) => setConfirmPass(e.target.value)}/>
+                </div>
+                <div className="form-group">
+                        <div className="custom-control custom checkbox">
+                            <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                            <label className="custom-control-label" htmlFor="customCheck1">I agree to the terms and conditions</label>
+                        </div>
+                    </div>
+                    <button
+                        type="submit"
+                        style={{ background: loader ? "#ccc" : " rgb(225, 225, 225)" }}
+                        onClick={doSignUp}
+                        to="/survey">
+                        Sign Up
+                    </button>
+                    <button className='border shadow-lg hover:shadow-xl px-2 py-1 relative flex items-center' to="/dashboard" onClick={doGoogleSignUp}><img border = "0" src={image} alt="Sign In with Google" width="24" height="24"></img></button>
+                   
+                <p className="forgot-password text-right">
+                    Already registered?: 
+                    <Button variant="outlined" href="/provider/login"><b>Log in here</b></Button>
+                </p>
+            </form>
+        </div>
+        </div>
+        /*
         <form className="form" onSubmit={handleSubmit}>
             <h3>Provider Registration</h3>
 
@@ -120,12 +197,12 @@ const SignUp = ({setShouldShowSignIn}) => {
 
                 <div className="form-group">
                     <label>Password (at least 6 characters)</label>
-                    <TextField label="Enter password" className="form-control" variant="outlined" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <TextField label="Enter password" className="form-control" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
                 </div>
 
                 <div className="form-group">
                     <label>Confirm Password (at least 6 characters)</label>
-                    <TextField label="Enter password again" className="form-control" variant="outlined" type="password" value={confirmPass} onChange={(e) => setConfirmPass(e.target.value)} />
+                    <TextField label="Enter password again" className="form-control" type="password" value={confirmPass} onChange={(e) => setConfirmPass(e.target.value)} />
                 </div>
 
                 <div className="form-group">
@@ -142,7 +219,7 @@ const SignUp = ({setShouldShowSignIn}) => {
                     to="/survey">
                     Register
                 </button>
-                {/* DOES NOT REDIRECT even with href */}
+                {/* DOES NOT REDIRECT even with href }
 
             </div>
 
@@ -150,7 +227,7 @@ const SignUp = ({setShouldShowSignIn}) => {
                 Already registered?: 
                 <Button variant="outlined" onClick={(e) => {e.preventDefault();history("/provider/login"); setShouldShowSignIn(true);}}><b>Log in here</b></Button>
             </p>
-        </form>
+        </form> */
     );
 };
 
