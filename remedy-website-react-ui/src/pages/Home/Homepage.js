4ae75538a7f042d8a535f7b41fb3fe4b222368b7
Kuira Edwards <kuiraedwards@gmail.com>
2023-04-04 17:18:18 -0400

    UI Changes

--- remedy-website-react-ui/src/pages/Home/Homepage.js
+++ remedy-website-react-ui/src/pages/Home/Homepage.js
@@ -18,28 +18,7 @@ const HomePage = ({ }) => {
             </form>
         </div>
     </div>
-        <div className="container-side-by-side">
-            <div className="left-image">
-            </div>
-            <div className="right-form"></div>
-        <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
             
-
-        <div className='bg-gray-100 flex flex-col justify-center'>
-            <form className='max-w-[400px] w-full mx-auto bg-white p-4'>
-                <h2 className='text-4xl font-bold text-center py-6'>HOME</h2>
-                <button type="submit" className="border shadow-lg hover:shadow-xl px-15 py-2 relative flex items-center"><a href="/provider">Provider</a></button>
-                <button type="submit" className="border shadow-lg hover:shadow-xl px-15 py-2 relative flex items-center"><a href="/patient">Patient</a></button>
-            </form>
-        </div>
-    </div>
-         {/* <form> <h3>Home Page</h3>
-            <button type="submit" className="btn btn-dark btn-lg btn-block"><a href="/provider">Provider</a></button>
-            <button type="submit" className="btn btn-dark btn-lg btn-block"><a href="/patient">Patient</a></button>
-             <button className='border w-full my-5 py-2 bg-indigo-600 hover:bg-indigo-500 text-white'onClick={doSignIn}>Sign In</button>
-        </form>*/}
-            
-        </div>
     );
 };
 
