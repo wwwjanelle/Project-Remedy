707dad56e9bd972d08347b94fdb5d7d6a6b88ad1
Manish Basnet <mnshbsnt@gmail.com>
2023-04-13 13:38:01 -0400

    Fixing merge errors

--- remedy-website-react-ui/src/pages/Secure/SendSurvey.js
+++ remedy-website-react-ui/src/pages/Secure/SendSurvey.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import {gapi} from "gapi-script";
+
 
 function SendSurvey() {
   const [data, setData] = useState([]);
